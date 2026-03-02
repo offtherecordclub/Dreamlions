38ab8c7a2e8bd6e4c8f87f02cf20261ebf4c1420
최윤경 <144264984+offtherecordclub@users.noreply.github.com>
2026-03-02 17:57:28 +0900

    위치수정

--- script.js
+++ script.js
@@ -2,24 +2,26 @@ const bin = document.querySelector(".bin");
 const items = document.querySelectorAll(".item");
 const text = document.querySelector(".text");
 
-// 컵 안에서의 최종 위치(px)
+// 컵 안에서의 최종 위치 (비율로 설정)
 const positions = [
-  { left: 180, top: 310 }, // green
-  { left: 50, top: 160 }, // pink
-  { left: 280, top: 25 }, // yellow
-  { left: 100, top: 20 }, // blue
+  { left: 0.45, top: 0.63 }, // green
+  { left: 0.1, top: 0.36 }, // pink
+  { left: 0.45, top: 0.03 }, // yellow
+  { left: 0.1, top: 0.05 }, // blue
 ];
 
 items.forEach((item, index) => {
+  const binRect = bin.getBoundingClientRect();
+
   // 시작 위치 (컵 위)
   item.style.top = "-150px";
 
-  // x 위치 먼저 세팅
-  item.style.left = positions[index].left + "px";
+  // left 위치 (컵 기준 비율)
+  item.style.left = binRect.width * positions[index].left + "px";
 
   setTimeout(() => {
     item.classList.add("show");
-    item.style.top = positions[index].top + "px";
+    item.style.top = binRect.height * positions[index].top + "px";
   }, index * 500);
 });
 
